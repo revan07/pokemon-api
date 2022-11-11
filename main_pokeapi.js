72905e030212b980c4f768a77c6a0ad172dacb2b
revan07 <52211114+revan07@users.noreply.github.com>
2022-11-11 06:29:08 +0100

    pokemon api fetch

---
+++ main_pokeapi.js
@@ -0,0 +1,23 @@
+var post = document.getElementById('post').addEventListener('click', data)
+
+
+
+function data() {
+    const name = document.getElementById('fetch').value
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((res) => res.json())
+        .then((data) => {
+
+            document.getElementById('boxContent').innerHTML = `
+           
+           <div>
+           <p>ID</p><br> 
+           <img src="${data.sprites.other["dream_world"].front_default}" alt="${data.name}">
+
+           </div>
+
+           `
+            console.log(data)
+        })
+
+};
